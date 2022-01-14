e4e8b58a38bac4118e54e97975233cf919ae9e9d
jonathanandre <joandre000@gmail.com>
2022-01-14 16:06:20 +0100

    ajout acces tournoi Gael

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ConnexionComponent } from './connexion/connexion.component';
+import { TournoiComponent } from './tournoi/tournoi.component';
 
 const routes: Routes = [
-  
+  {path: 'tournoi', component: TournoiComponent}
 ];
 
 @NgModule({
