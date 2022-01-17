be375e9632679d0b28424909261482d1a51e723b
cyrillefruchard <cyrille.fruchard@yahoo.fr>
2022-01-17 14:25:48 +0100

    ami

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MonProfilComponent } from './mon-profil/mon-profil.component';
 import { TournoiComponent } from './tournoi/tournoi.component';
+import { PotesComponent } from './potes/potes.component';
 
 const routes: Routes = [
   {path: 'tournoi', component: TournoiComponent},
-  {path: 'mon-profil', component: MonProfilComponent}
+  {path: 'mon-profil', component: MonProfilComponent},
+  {path: 'potes', component: PotesComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,8 @@ import { InscriptionComponent } from './inscription/inscription.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import {MatIconModule} from '@angular/material/icon';
-import { MonProfilComponent } from './mon-profil/mon-profil.component'; 
+import { MonProfilComponent } from './mon-profil/mon-profil.component';
+import { PotesComponent } from './potes/potes.component'; 
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { MonProfilComponent } from './mon-profil/mon-profil.component';
     ConnexionComponent,
     ProfilComponent,
     InscriptionComponent,
-    MonProfilComponent
+    MonProfilComponent,
+    PotesComponent
   ],
   imports: [
     BrowserModule,
