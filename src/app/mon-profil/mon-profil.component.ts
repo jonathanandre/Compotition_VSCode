2a7d5bb67590fbfc71a8d0f4edd43f2be999c7ad
jonathanandre <joandre000@gmail.com>
2022-01-18 15:57:39 +0100

    fix little bug refresh

--- src/app/mon-profil/mon-profil.component.ts
+++ src/app/mon-profil/mon-profil.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { ModifProfilComponent } from './modif-profil/modif-profil.component';
 
@@ -10,7 +11,7 @@ import { ModifProfilComponent } from './modif-profil/modif-profil.component';
 })
 export class MonProfilComponent implements OnInit {
   user :any;
-  constructor(private auth: AuthService, private dialog: MatDialog) { }
+  constructor(private auth: AuthService, private dialog: MatDialog, private router: Router) { }
 
   ngOnInit(): void {
     this.user = this.auth.getUserConnect();
@@ -18,6 +19,16 @@ export class MonProfilComponent implements OnInit {
 
   callModifProfil() {
     const myDialog = this.dialog.open(ModifProfilComponent);
+    myDialog.afterClosed().subscribe(result => {
+      this.reloadComponent();
+    });
+  }
+
+  reloadComponent() {
+    let currentUrl = this.router.url;
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    this.router.onSameUrlNavigation = 'reload';
+    this.router.navigate([currentUrl]);
   }
 
 }
